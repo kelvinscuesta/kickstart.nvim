d8f5908295d333b81e9ffffa106237d5c90a8379
Kelvin Cuesta <kelvin.cuesta@gusto.com>
2025-03-29 11:06:23 -0700

    add virtual lines

--- init.lua
+++ init.lua
@@ -5,5 +5,7 @@ require 'config.keymap'
 require 'config.options'
 require 'config.lazy'
 
+vim.diagnostic.config { virtual_lines = true }
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
