8689e8b75fd8a93dcaca8017fa5d6596f4c3b23b
Kelvin Cuesta <kelvin.cuesta@gusto.com>
2025-03-31 16:35:36 -0700

    bring back lua line

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,4 +1,4 @@
---[[ return {
+return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
@@ -44,5 +44,4 @@
       extensions = {},
     }
   end,
-}--]]
-return {}
+}
